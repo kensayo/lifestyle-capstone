913e3e37a11575292d68f7e2af9b1af839db02cb
Kenny Ortega <kennyaoc@gmail.com>
2021-08-15 19:22:05 -0400

    Add header and main post view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'post#index'
-  resources :posts, only: %i[index create]
+  resources :posts, only: %i[index create show update]
 end
