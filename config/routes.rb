55590ba286bbace4a304fcedbe5170ef05e9f06a
Kenny Ortega <kennyaoc@gmail.com>
2021-08-17 19:37:00 -0400

    Complete sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'post#index'
-  resources :posts, only: %i[index create show update]
+  root to: 'recipe#index'
+  resources :recipes, only: %i[index create show update]
 end
